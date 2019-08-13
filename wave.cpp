75097be93389a336732e88d8dfc13e2216fdd1c2
KhamliukElena <54072915+KhamliukElena@users.noreply.github.com>
2019-08-13 19:31:47 +0300

    Update wave.cpp

--- wave.cpp
+++ wave.cpp
@@ -2,72 +2,72 @@
 #include <cstdlib>
 #include <ctime>
 
-int** genGraph(int n, int** A)
+int** genGraph(int matrixSize, int** gridField)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < matrixSize; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < matrixSize; j++)
         {
-            A[i][j] = rand() % 2;
+            gridField[i][j] = rand() % 2;
         }
     }
-    return A;
+    return gridField;
 }
 
-void outGraph(int n, int** A)
+void outGraph(int matrixSize, int** gridField)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < matrixSize; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < matrixSize; j++)
         {
-            std::cout<<A[i][j]<<" ";
+            std::cout<<gridField[i][j]<<" ";
         }
         std::cout<<std::endl;
     }
 }
 
-int** reorganizeGraph(int n, int** A)
+int** reorganizeGraph(int matrixSize, int** gridField)
 {
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < matrixSize; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < matrixSize; j++)
         {
-            if(A[i][j]==0)
-                A[i][j]=-2;
+            if(gridField[i][j]==0)
+                gridField[i][j]=-2;
             else
-                A[i][j]=-1;
+                gridField[i][j]=-1;
         }
     }
-    return A;
+    return gridField;
 }
 
-int lee(int inX, int inY, int tgX, int tgY, int n, int** A) //Lee alg
+int lee(int inX, int inY, int tgX, int tgY, int matrixSize, int** gridField) //Lee alg
 {
     int dx[4] = {1, 0, -1, 0}; //right, up, left, down
     int dy[4] = {0, 1, 0, -1};
     const int blocked = -1, notVisited = -2;
     bool stop;
-    if (A[inX][inY] == blocked || A[tgX][tgY] == blocked) return -1; //if start or finish is blocked
+    if (gridField[inX][inY] == blocked || gridField[tgX][tgY] == blocked) return -1; //if start or finish is blocked
     if (inX == tgX && tgY == inX) return 0; //if start equals to finish
     int d = 0;
-    A[inX][inY] = 0;
+    gridField[inX][inY] = 0;
     do
     {
         stop = true;
-        for (int x = 0; x<n; x++)
+        for (int x = 0; x<matrixSize; x++)
         {
-            for (int y=0; y<n; y++)
+            for (int y=0; y<matrixSize; y++)
             {
-                if (A[x][y] == d)
+                if (gridField[x][y] == d)
                 {
                     for (int k=0; k<4; k++)
                     {
                         int newX = x + dx[k];
                         int newY = y + dy[k];
-                        if (newX < n && newY < n && newX>=0 && newY>=0 && A[newX][newY] == notVisited)
+                        if (newX < matrixSize && newY < matrixSize && newX>=0 && newY>=0 && gridField[newX][newY] == notVisited)
                         {
                             stop = false;
-                            A[newX][newY] = d + 1;
+                            gridField[newX][newY] = d + 1;
                         }
                     }
                 }
@@ -75,12 +75,12 @@ int lee(int inX, int inY, int tgX, int tgY, int n, int** A) //Lee alg
         }
         d++;
     }
-    while(!stop && A[tgX][tgY] == notVisited);
-    if (A[tgX][tgY]==notVisited) //no path found
+    while(!stop && gridField[tgX][tgY] == notVisited);
+    if (gridField[tgX][tgY]==notVisited) //no path found
         return -1;
     else //restore the path
     {
-        d = A[tgX][tgY];
+        d = gridField[tgX][tgY];
         int kPrev = -1; //direction of previous step
         int x = tgX, y = tgY, moves = 1;
         while (d>0)
@@ -90,7 +90,7 @@ int lee(int inX, int inY, int tgX, int tgY, int n, int** A) //Lee alg
             {
                 int newX = x + dx[k];
                 int newY = y + dy[k];
-                if ( newY >= 0 && newY < n && newX >= 0 && newX < n && A[newX][newY] == d)
+                if ( newY >= 0 && newY < matrixSize && newX >= 0 && newX < matrixSize && gridField[newX][newY] == d)
                 {
                     x = x + dx[k];
                     y = y + dy[k];
@@ -105,7 +105,7 @@ int lee(int inX, int inY, int tgX, int tgY, int n, int** A) //Lee alg
     }
 }
 
-void searchPath(int n, int** A)
+void searchPath(int matrixSize, int** gridField)
 {
     try
     {
@@ -114,31 +114,31 @@ void searchPath(int n, int** A)
         try
         {
             std::cin>>inX>>inY;
-            if (inX < 0 || inY<0 || inX>=n || inY>=n)
+            if (inX < 0 || inY<0 || inX>=matrixSize || inY>=matrixSize)
                 throw 1;
         }
         catch(int ex)
         {
             std::cout<<"Error!"<<std::endl;
-            while(inX < 0 || inY<0 || inX>=n || inY>=n)
+            while(inX < 0 || inY<0 || inX>=matrixSize || inY>=matrixSize)
                 std::cin>>inX>>inY;
         }
         std::cout<<"Please insert coordinates of target cell: "<<std::endl;
         try
         {
             std::cin>>tgX>>tgY;
-            if (tgX < 0 || tgY<0 || tgX>=n || tgY>=n)
+            if (tgX < 0 || tgY<0 || tgX>=matrixSize || tgY>=matrixSize)
                 throw 2;
         }
         catch(int ex)
         {
-            while (tgX < 0 || tgY<0 || tgX>=n || tgY>=n)
+            while (tgX < 0 || tgY<0 || tgX>=matrixSize || tgY>=matrixSize)
                     std::cin>>tgX>>tgY;
         }
-        //we are going to use Lee (или волновой) algorithm, that's why it's better to reorganize our matrix to make code easier
+        //we are going to use Lee algorithm, that's why it's better to reorganize our matrix to make code easier
         //blocked cells will be marked as -1; empty not visited - -2
-        A = reorganizeGraph(n, A);
-        int moves = lee(inX, inY, tgX, tgY, n, A);
+        gridField = reorganizeGraph(matrixSize, gridField);
+        int moves = lee(inX, inY, tgX, tgY, matrixSize, gridField);
         std::cout<<"Answer: "<<moves<<std::endl;
     }
     catch(...)
@@ -149,50 +149,50 @@ void searchPath(int n, int** A)
 
 int main()
 {
-    int n; // matrix size
-    int** A;
+    int matrixSize; // matrix size
+    int** gridField;
     std::cout<<"Press 1 for test example, any other letter/number for generating random grid "<<std::endl;
     char mode;
     std::cin>>mode;
     if (mode=='1')
     {
-        n = 3;
-        A = new int *[n]; //A
-        for (int i = 0; i < n; i++)
-            A[i] = new int [n];
-        for (int i=0; i<n; i++)
+        matrixSize = 3;
+        gridField = new int *[matrixSize]; //A
+        for (int i = 0; i < matrixSize; i++)
+            gridField[i] = new int [matrixSize];
+        for (int i=0; i<matrixSize; i++)
         {
-            for (int j=0; j<n; j++)
-                A[i][j] = 0;
+            for (int j=0; j<matrixSize; j++)
+                gridField[i][j] = 0;
         }
-        A[1][1] = 1;
-        A[1][0] = 1;
+        gridField[1][1] = 1;
+        gridField[1][0] = 1;
         std::cout<<"Example grid: "<<std::endl;
     }
     else
     {
         std::cout<<"Please, specify size of grid (S): ";
-        if(!(std::cin>>n && n>1))
+        if(!(std::cin>>matrixSize && matrixSize>1))
         {
             std::cout<<"Wrong input!"<<std::endl;
-            while(n<2)
-                std::cin>>n;
+            while(matrixSize<2)
+                std::cin>>matrixSize;
         }
-        A = new int *[n]; //A
-        for (int i = 0; i < n; i++)
-            A[i] = new int [n];
+        gridField = new int *[matrixSize]; //A
+        for (int i = 0; i < matrixSize; i++)
+            gridField[i] = new int [matrixSize];
         srand(time(0));
-        A = genGraph(n, A); //generate a random graph
+        gridField = genGraph(matrixSize, gridField); //generate a random graph
         std::cout<<"Generated grid: "<<std::endl;
     }
 
-    outGraph(n, A);
+    outGraph(matrixSize, gridField);
 
-    searchPath(n, A);
+    searchPath(matrixSize, gridField);
 
-    for (int i = 0; i < n; i++)
-        delete[] A[i];
-    delete [] A;
+    for (int i = 0; i < matrixSize; i++)
+        delete[] gridField[i];
+    delete [] gridField;
 
     return 0;
 }
